cffadcb580b1969f8045ac6dfd9837aab6ef155b
vikaskawale <100497780+vikaskawale@users.noreply.github.com>
2022-11-08 11:36:27 +1100

    Update plusminus.py

--- plusminus.py
+++ plusminus.py
@@ -1,7 +1,7 @@
 '''Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
 
 Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4 are acceptable.'''
-
+# Vikas K.
 import math
 import os
 import random
