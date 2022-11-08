39ea5ed11d60e7cf277493b31ca0059d08837af5
vikaskawale <100497780+vikaskawale@users.noreply.github.com>
2022-11-08 11:35:50 +1100

    Add files via upload

---
+++ plusminus.py
@@ -0,0 +1,38 @@
+'''Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4 are acceptable.'''
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    pos=0
+    neg=0
+    neu=0
+    for i in range(len(arr)):
+        if(arr[i]>0):
+            pos+=1
+        elif(arr[i]==0):
+            neu+=1
+        else:
+            neg+=1
+    print(pos/len(arr))
+    print(neg/len(arr))
+    print(neu/len(arr))
+
+    #n = int(input())
+
+
+arr = list(map(int, input('Enter numbers for list: ').rstrip().split()))
+    
+
+plusMinus(arr)
