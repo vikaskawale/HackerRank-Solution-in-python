c1639b66f75cad60eb5b3d4bd28e6ed784251af6
vikaskawale <vikaskawale15@gmail.com>
2022-12-02 00:28:51 +1100

    push changes

--- Cal.py
+++ Cal.py
@@ -17,23 +17,39 @@ elif operator == '/':
 
 print('Answer: ', result)
 
+
 nextOpeation = input('Do you want to perform more opration in this calculation: (y, n): ')
 
-if nextOpeation == 'y':
-    operator = input('which operation to perform (+, -, *, /): ')
-    num3 = float(input('Enter third number: '))
-    if operator == '+':
-        result = result + num3
-    elif operator == '-':
-        result = result - num3
-    elif operator == '*':
-        result = result * num3
-    elif operator == '/':
-        result = result / num3
-    print('Answer: ', result)
 
-else:
-    print('Answer: ', result)
+while True:
+    if nextOpeation == 'y':
+        operator = input('which operation to perform (+, -, *, /): ')
+        num3 = float(input('Enter next number: '))
+
+        if operator == '+':
+            result = result + num3
+            print('Answer: ', result)
+            nextOpeation = input('Do you want to perform more opration in this calculation: (y, n): ')
+            
+        elif operator == '-':
+            result = result - num3
+            print('Answer: ', result)
+            nextOpeation = input('Do you want to perform more opration in this calculation: (y, n): ')
+            
+        elif operator == '*':
+            result = result * num3
+            print('Answer: ', result)
+            nextOpeation = input('Do you want to perform more opration in this calculation: (y, n): ')
+            
+        elif operator == '/':
+            result = result / num3
+            print('Answer: ', result)
+            nextOpeation = input('Do you want to perform more opration in this calculation: (y, n): ')
+            
+
+
+    else:
+        break
 
 print('Thank you for using this Calculator')
 
---
+++ Javascript/first.js
@@ -0,0 +1,17 @@
+console.log('Hello Vikas, This is your first javaScript code')
+let a =67
+console.log(a)
+a = 'Vikas'
+console.log(a)
+//let 8vikas = 15 --> not alowed as variable name can not start with number
+
+var x = 75
+var y = 'Vikas'
+var z = undefined
+const author = 'vikas'
+//let author = 'harry'
+{
+    let y = 'Harry'
+    console.log(y)
+}
+console.log(y)
